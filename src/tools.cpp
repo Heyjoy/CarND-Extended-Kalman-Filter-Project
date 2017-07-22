de04c1e1e16b2de03e930fbfc1ee845eb0266a0f
Heyjoy <iheyjoy@gmail.com>
2017-07-22 18:40:31 +0800

    start point

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,6 +15,20 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+	VectorXd rmse(estimations[0].size());
+	VectorXd sum(estimations[0].size());
+
+	sum.fill(0);
+	for (int i = 0; i<estimations.size(); i++)
+	{
+		VectorXd error = estimations[i] - ground_truth[i];
+		VectorXd error2 = error.array()*error.array();
+		sum += error2;
+	}
+	rmse = (sum / estimations.size()).array().sqrt();
+
+	//cout <<"size is :" <<estimations.size()<< ground_truth.size() << endl;
+	return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -22,4 +36,6 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+	Eigen::MatrixXd x;
+	return x;
 }
