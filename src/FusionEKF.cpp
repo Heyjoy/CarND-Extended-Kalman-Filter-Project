af8c1070ff429b9059397701c5bf77a15e46269e
Heyjoy <iheyjoy@gmail.com>
2017-08-02 00:48:23 +0800

    fix the update part

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -144,8 +144,11 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   ekf_.F_(0, 2) = dt;
   ekf_.F_(1, 3) = dt;
-
-  //ekf_.Predict();
+  ekf_.Q_ << dt_4 / 4 * noise_ax, 0, dt_3 / 2 * noise_ax, 0,
+	  0, dt_4 / 4 * noise_ay, 0, dt_3 / 2 * noise_ay,
+	  dt_3 / 2 * noise_ax, 0, dt_2*noise_ax, 0,
+	  0, dt_3 / 2 * noise_ay, 0, dt_2*noise_ay;
+  ekf_.Predict();
 
   /*****************************************************************************
    *  Update
@@ -159,11 +162,14 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
-	  ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
+	  ekf_.R_ = R_radar_;
+	  Hj_ = tools.CalculateJacobian(ekf_.x_);
+	  ekf_.H_ = Hj_;
 	  ekf_.UpdateEKF(measurement_pack.raw_measurements_);
 
   } else {
     // Laser updates
+	  ekf_.R_ = R_laser_;
 	  ekf_.H_ = H_laser_;
 	  ekf_.Update(measurement_pack.raw_measurements_);
   }
